fdc98d49a5abcff4f58a010201b49167c616c2d1
EunjinLee08 <eunjinlee0128@gmail.com>
2026-07-28 16:07:39 +0900

    feat: Rust 백엔드 기초 구성

---
+++ backend-eunjin/todo-api/src/main.rs
@@ -0,0 +1,19 @@
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8082));
+    println!("Listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8082")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
